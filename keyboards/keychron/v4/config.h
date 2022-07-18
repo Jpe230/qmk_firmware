40e8776347fb03e5b34aef9a9367700e59f1f2e5
pablin.123.ra@gmail.com <pablin.123.ra@gmail.com>
2022-07-18 14:28:32 -0500

    Enable WL driver for all Keychron boards

--- keyboards/keychron/v4/config.h
+++ keyboards/keychron/v4/config.h
@@ -60,6 +60,10 @@
 /* We have 2KB EEPROM size on STM32L432 */
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 2047
 
+/* EEPROM Driver Configuration */
+#define WEAR_LEVELING_LOGICAL_SIZE 2048
+#define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)
+
 // RGB Matrix Animation modes. Explicitly enabled
 // For full list of effects, see:
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
