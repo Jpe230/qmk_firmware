8d69fe2d714de9e7a9a37c3aa2001e0fc406b0fd
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-10-27 22:16:09 +0300

    bastards: build eeprom driver

--- keyboards/keychron/k14/rules.mk
+++ keyboards/keychron/k14/rules.mk
@@ -18,8 +18,11 @@ CUSTOM_MATRIX     = no
 DIP_SWITCH_ENABLE = yes
 
 # Custom RGB matrix handling
- RGB_MATRIX_ENABLE = yes
- RGB_MATRIX_DRIVER = SN32F24xB
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = SN32F24xB
+
+ EEPROM_DRIVER = wear_leveling
+ WEAR_LEVELING_DRIVER = sn32_flash
 
 # # project specific files
 # Build ansi by default
